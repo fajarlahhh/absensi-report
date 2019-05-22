ed001d81750d4f7801a7705190eca5512291fd41
Andi Fajar Nugraha <andifajarlah@gmail.com>
2019-05-22 10:31:26 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,28 @@ Route::group(['middleware' => ['auth']], function () {
 		Route::get('/datapengguna', 'PenggunaController@index')->name('datapengguna');
 		Route::get('/datapengguna/edit/{post}', 'PenggunaController@edit')->middleware(['role:administrator|user']);
 		Route::get('/datapengguna/tambah', 'PenggunaController@tambah')->middleware(['role:administrator|user']);
-		Route::post('/datapengguna/tambah', 'PenggunaController@do_tambah'->middleware(['role:administrator|user']));
-		Route::post('/datapengguna/edit', 'PenggunaController@do_edit'->middleware(['role:administrator|user']));
-		Route::get('/datapengguna/hapus/{post}', 'PenggunaController@hapus'->middleware(['role:administrator|user']));
+		Route::post('/datapengguna/tambah', 'PenggunaController@do_tambah')->middleware(['role:administrator|user']);
+		Route::post('/datapengguna/edit', 'PenggunaController@do_edit')->middleware(['role:administrator|user']);
+		Route::get('/datapengguna/hapus/{post}', 'PenggunaController@hapus')->middleware(['role:administrator|user']);
 	});
 
 	Route::group(['middleware' => ['auth', 'permission:data mesin']], function () {
 		Route::get('/datamesin', 'DatamesinController@index')->name('datamesin');
 		Route::get('/datamesin/edit/{post}', 'DatamesinController@edit')->middleware(['role:administrator|user']);
-		Route::get('/datamesin/tambah', 'DatamesinController@tambah'->middleware(['role:administrator|user']));
-		Route::post('/datamesin/tambah', 'DatamesinController@do_tambah'->middleware(['role:administrator|user']));
-		Route::post('/datamesin/edit', 'DatamesinController@do_edit'->middleware(['role:administrator|user']));
-		Route::get('/datamesin/hapus/{post}', 'DatamesinController@hapus'->middleware(['role:administrator|user']));
+		Route::get('/datamesin/tambah', 'DatamesinController@tambah')->middleware(['role:administrator|user']);
+		Route::post('/datamesin/tambah', 'DatamesinController@do_tambah')->middleware(['role:administrator|user']);
+		Route::post('/datamesin/edit', 'DatamesinController@do_edit')->middleware(['role:administrator|user']);
+		Route::get('/datamesin/hapus/{post}', 'DatamesinController@hapus')->middleware(['role:administrator|user']);
 	});
 
 	Route::group(['middleware' => ['auth', 'permission:data anggota']], function () {
 		Route::get('/dataanggota', 'Dataanggotacontroller@index')->name('dataanggota');
-		Route::get('/dataanggota/edit/{post}', 'Dataanggotacontroller@edit'->middleware(['role:administrator|user']));
-		Route::get('/dataanggota/tambah', 'Dataanggotacontroller@tambah'->middleware(['role:administrator|user']));
-		Route::post('/dataanggota/tambah', 'Dataanggotacontroller@do_tambah'->middleware(['role:administrator|user']));
-		Route::post('/dataanggota/edit', 'Dataanggotacontroller@do_edit'->middleware(['role:administrator|user']));
-		Route::get('/dataanggota/hapus/{post}', 'Dataanggotacontroller@hapus'->middleware(['role:administrator|user']));
-		Route::get('/dataanggota/upload', 'Dataanggotacontroller@upload'->middleware(['role:administrator|user']));
+		Route::get('/dataanggota/edit/{post}', 'Dataanggotacontroller@edit')->middleware(['role:administrator|user']);
+		Route::get('/dataanggota/tambah', 'Dataanggotacontroller@tambah')->middleware(['role:administrator|user']);
+		Route::post('/dataanggota/tambah', 'Dataanggotacontroller@do_tambah')->middleware(['role:administrator|user']);
+		Route::post('/dataanggota/edit', 'Dataanggotacontroller@do_edit')->middleware(['role:administrator|user']);
+		Route::get('/dataanggota/hapus/{post}', 'Dataanggotacontroller@hapus')->middleware(['role:administrator|user']);
+		Route::get('/dataanggota/upload', 'Dataanggotacontroller@upload')->middleware(['role:administrator|user']);
 	});
 });
 
